8f358207a3b47476661556fe2bac4c490359b6a4
peshekhonovaas <peshekhonova_anastasiia@epam.com>
2025-02-11 14:02:12 +0100

    Add a Dockerfiles and a Docker Compose file

---
+++ init-scripts/resource-db/init.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS audio_file_details (
+    id serial PRIMARY KEY,
+    name VARCHAR (2000) NOT NULL
+);
---
+++ init-scripts/song-db/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS song_meta_data (
+    id NUMERIC PRIMARY KEY NOT NULL,
+    name VARCHAR (100) NOT NULL,
+    artist VARCHAR (100) NOT NULL,
+    album VARCHAR (100) NOT NULL,
+    duration VARCHAR (100) NOT NULL,
+    year VARCHAR (100) NOT NULL
+);
